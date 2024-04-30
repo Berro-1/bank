811ca3f77af0edc3c48122d6b8298ad6022340c4
karniib <hadikarnib03@gmail.com>
2024-05-01 00:33:42 +0300

    cust

---
+++ backend/models/Customers.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const customerSchema = new Schema(
+  {
+    // Assuming customer_ID is a regular ObjectId primary key
+    customer_ID: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phone_number: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Customer", customerSchema);
