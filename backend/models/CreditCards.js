201ec07084c24f83112ce8591f57de5446a33954
karniib <hadikarnib03@gmail.com>
2024-05-01 11:43:30 +0300

    creditcards

---
+++ backend/models/CreditCards.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const creditCardsSchema = new Schema(
+  {
+    card_name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    customer_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
+    },
+    expiry_date: {
+      type: Date,
+      required: true,
+    },
+    credit_limit: {
+      type: Number,
+      required: true,
+    },
+    available_credit: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("CreditCards", creditCardsSchema);
