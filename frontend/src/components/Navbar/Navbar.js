0f44768ff0e1aa37aacf7413926070a6e52a4bb4
karniib <hadikarnib03@gmail.com>
2024-05-03 20:35:12 +0300

    stroke color

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -44,7 +44,7 @@ export default function Navbar() {
           className="md:hidden text-white focus:outline-none focus:ring-2 focus:ring-green-500 p-2"
         >
           <svg
-            className="w-6 h-6"
+            className="w-6 h-6  hover:stroke-green-500"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
