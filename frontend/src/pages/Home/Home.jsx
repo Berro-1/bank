f43b505e2b8616c2d6b9a8adcae1d145e81d136f
Berro-1 <hussienberro1@gmail.com>
2024-05-03 23:09:59 +0300

    ..

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-center'>Welcome to our website!</h1>
+    <div className="w-full flex flex-wrap justify-around items-center bg-pink-300 py-12">
+      <div className="text-center p-4">
+        <div className="w-40 h-40 flex items-center justify-center rounded bg-white mb-4">
+          <div className=""></div>
+        </div>
+      </div>
+      <div className="text-center p-4">
+        <div className="w-40 h-40 flex items-center justify-center rounded bg-white mb-4">
+          <img src="/path-to-your-icon/business-icon.png" alt="Business Development"/>
+        </div>
+      </div>
+      <div className="text-center p-4">
+        <div className="w-40 h-40 flex items-center justify-center rounded bg-white mb-4">
+          <img src="/path-to-your-icon/capacity-icon.png" alt="Capacity Building & Organizational Development"/>
+        </div>
+      </div>
+      <div className="text-center p-4">
+        <div className="w-40 h-40 flex items-center justify-center rounded bg-white mb-4">
+          <img src="/path-to-your-icon/social-icon.png" alt="Social & Communication Skills"/>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
