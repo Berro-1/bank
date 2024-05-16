af1079061518ab7ca1b720d9e590c582fa71f55f
karniib <hadikarnib03@gmail.com>
2024-05-16 10:49:26 +0300

    root

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -3,12 +3,14 @@ import TestsSlice from './testing/testReducers';
 import transactionSlice from "./Transactions/transactionSlice";
 import allAccountsSlice from "./allAccounts/allAccountsSlice";
 import cardsSlice from "./cards/cardsSlice";
+import loanSlice from "./Loans/loansSlice";
 
 const rootReducer = combineReducers({
   Tests: TestsSlice.reducer,
   transactions: transactionSlice.reducer,
   accounts: allAccountsSlice.reducer,
-  cards:cardsSlice.reducer
+  cards:cardsSlice.reducer,
+  loans: loanSlice.reducer
 });
 
 export default rootReducer;
