9ba503894bba9225e263e4d0aa98125c32a9b25c
Berro-1 <hussienberro1@gmail.com>
2024-06-02 22:52:32 +0300

    latesttransactions

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -153,165 +153,6 @@ const createTransaction = async (req, res) => {
   }
 };
 
-const getLatestTransactions = async (req, res) => {
-  const { accountId } = req.params;
-  try {
-    // Check if the accountId exists in Accounts, Loans, or Credit Cards
-    const accountExists = await Account.findById(accountId).lean().exec();
-    const loanExists = await Loan.findById(accountId).lean().exec();
-    const creditCardExists = await CreditCard.findById(accountid).lean().exec();
-
-    if (!accountExists && !loanExists && !creditCardExists) {
-      return res
-        .status(404)
-        .json({ error: "Account, loan, or credit card not found" });
-    }
-
-    // Find transactions for the given accountId
-    const transactions = await Transaction.find({
-      $or: [{ account: accountId }, { second_account: accountId }],
-    })
-      .populate({
-        path: "account",
-        populate: {
-          path: "user",
-          select: "name",
-        },
-      })
-      .populate({
-        path: "second_account",
-        populate: {
-          path: "user",
-          select: "name",
-          match: { _id: { $exists: true } },
-        },
-      })
-      .sort({ createdAt: -1 })
-      .limit(5)
-      .lean()
-      .exec();
-
-    const enhancedTransactions = await Promise.all(
-      transactions.map(async (transaction) => {
-        if (!transaction.second_account) {
-          // Attempt to find in CreditCards or Loans if not populated
-          const card = await CreditCard.findById(transaction.second_account)
-            .lean()
-            .exec();
-          const loan = card
-            ? null
-            : await Loan.findById(transaction.second_account).lean().exec();
-          transaction.second_account = card || loan || "Not found";
-        }
-        return transaction;
-      })
-    );
-
-    if (!enhancedTransactions.length) {
-      return res.status(404).json({
-        error: "No transactions found for this account, loan, or credit card",
-      });
-    }
-
-    res.status(200).json(enhancedTransactions);
-  } catch (error) {
-    console.error("Error finding transactions:", error);
-    res.status(500).json({ error: "Server error: " + error.message });
-  }
-};
-// const getAllTransactions = async (req, res) => {
-//   const { accountId } = req.params;
-//   try {
-//     const transactions = await Transaction.find({ account: accountId })
-//       .populate({
-//         path: "account",
-//         populate: {
-//           path: "user",
-//           select: "name",
-//         },
-//       })
-//       .sort({ createdAt: -1 })
-//       .lean()
-//       .exec();
-
-//     const detailedTransactions = await Promise.all(
-//       transactions.map(async (transaction) => {
-//         // Initialize second_account_info to 'Not found' by default
-//         let second_account_info = "Not found";
-
-//         // First, attempt to populate from Account
-//         const accountDetails = await Account.findById(
-//           transaction.second_account
-//         )
-//           .populate({
-//             path: "user",
-//             select: "name email -_id",
-//           })
-//           .lean()
-//           .exec();
-
-//         if (accountDetails) {
-//           second_account_info = {
-//             type: "Account",
-//             accountDetails: {
-//               ...accountDetails,
-//               user: accountDetails.user,
-//             },
-//           };
-//         } else {
-//           // If not an account, check if it's a Credit Card
-//           const cardDetails = await CreditCard.findById(
-//             transaction.second_account
-//           )
-//             .populate("user", "name email -_id")
-//             .lean()
-//             .exec();
-
-//           if (cardDetails) {
-//             second_account_info = {
-//               type: "Credit Card",
-//               cardDetails: {
-//                 cardName: cardDetails.card_name,
-//                 user: cardDetails.user,
-//               },
-//             };
-//           } else {
-//             // Lastly, check if it's a Loan
-//             const loanDetails = await Loan.findById(transaction.second_account)
-//               .populate("user", "name email -_id")
-//               .lean()
-//               .exec();
-
-//             if (loanDetails) {
-//               second_account_info = {
-//                 type: "Loan",
-//                 loanDetails: {
-//                   loanType: loanDetails.loan_type,
-//                   user: loanDetails.user,
-//                 },
-//               };
-//             }
-//           }
-//         }
-
-//         // Attach the populated second_account_info to the transaction
-//         transaction.second_account_info = second_account_info;
-//         return transaction;
-//       })
-//     );
-
-//     if (!detailedTransactions.length) {
-//       return res
-//         .status(404)
-//         .json({ error: "No transactions found for this account" });
-//     }
-
-//     res.status(200).json(detailedTransactions);
-//   } catch (err) {
-//     console.error("Error finding transactions:", err);
-//     res.status(500).json({ error: "Server error: " + err.message });
-//   }
-// };
 const getAllTransactions = async (req, res) => {
   const { id } = req.params;
 
@@ -319,7 +160,7 @@ const getAllTransactions = async (req, res) => {
     // Retrieve all transactions where the account or credit card is either the sender or receiver
     const transactions = await Transaction.find({
       $or: [{ sender: id }, { receiver: id }],
-    });
+    }).sort({ createdAt: -1 }).limit(5);
 
     // Process the transactions to include user names
     const detailedTransactions = await Promise.all(
@@ -342,6 +183,37 @@ const getAllTransactions = async (req, res) => {
         };
       })
     );
+
+    res.status(200).json(detailedTransactions);
+  } catch (err) {
+    console.error("Error fetching transactions:", err);
+    res.status(500).json({ error: "Server error: " + err.message });
+  }
+};
+
+const getLatestTransactions = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const transactions = await Transaction.find({
+      $or: [
+        { sender: id },
+        { receiver: id }
+      ]
+    }).sort({ createdAt: -1 }).limit(5);
+
+    const detailedTransactions = await Promise.all(transactions.map(async (transaction) => {
+      const sender = await getEntityDetails(transaction.sender, transaction.senderModel);
+      const receiver = await getEntityDetails(transaction.receiver, transaction.receiverModel);
+
+      return {
+        ...transaction._doc,
+        senderName: sender ? sender.user.name : null,
+        receiverName: receiver ? receiver.user.name : null,
+        senderUser: sender ? sender.user : null,
+        receiverUser: receiver ? receiver.user : null
+      };
+    }));
 
     res.status(200).json(detailedTransactions);
   } catch (err) {
