5d9d3c242ab5d3f14f4a25c3ac8694c50c91c91f
karniib <hadikarnib03@gmail.com>
2024-06-10 19:12:08 +0300

    fix

--- mobile/App.js
+++ mobile/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Signup from './screens/signup';
 import LoginScreen from './screens/loginScreen'
+import MainPage from './screens/mainPage';
 import Tabs from './components/tabs'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Tabs/>
+       {/* <Tabs/>  */}
+      <MainPage/>
       <StatusBar style="auto" />
     </View>
   );
